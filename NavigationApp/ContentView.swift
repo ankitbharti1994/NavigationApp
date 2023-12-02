fcacb2c0166bcd361557578efddb1a9ec37b2508
ankit bharti <ankitbharti1994@gmail.com>
2023-12-02 13:16:38 +0530

    Initial Commit

---
+++ NavigationApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NavigationApp
+//
+//  Created by ankit bharti on 02/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationApp/NavigationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationAppApp.swift
+//  NavigationApp
+//
+//  Created by ankit bharti on 02/12/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
